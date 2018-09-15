82b4440718b08554c0b990492bcf16381210b36d
Carlos Hernandez <37983005+polygonwind0w@users.noreply.github.com>
2018-09-15 05:38:00 -0700

    Framework upload
    Uplaod of node.js and angular frameworks

---
+++ ProjectTNBackEnd/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+// Tells the server to listen to incoming request on the specefied port
+app.listen(8000, () => {console.log('Server started!');});
+
+
+//test code
+// example of call back method
+app.route('/api/cats').get((req, res) => {
+    res.send({
+        cats: [{name: 'lilly'}, {name: 'LOOOOL lucy'}]
+    });
+});
+//example parameter and return
+app.route('/api/cats/:name').get((req, res) => {
+    const requestedCatName = req.params['name'];
+    res.send({name: requestedCatName});
+});
+//example bodyparser
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.route('/api/cats').post((req, res) => {
+    res.send(201, req.body);
+});
+//example put
+app.route('/api/cats/:name').put((req, res) => {
+    res.send(200, req.body);
+});
+//example delete
+app.route('/api/cats/:name').delete((req, res) => {
+    res.sendStatus(204);
+});
+
+var corsOptions = {
+    origin: 'http://example.com',
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOptions))
