e8223970e9301903735a2d2ab1297a3d391e30b0
firely111 <firely111@hotmail.com>
2018-09-20 13:12:47 -0700

    Change Output from XML to JSON

--- projectFrontEnd/src/app/input/ebay.service.ts
+++ projectFrontEnd/src/app/input/ebay.service.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
 })
 export class EbayService {
   // Ebay Api to search for any videoGame in the videoGame category
-  apiRoot = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=AronHubb-gamefax-PRD-8820665a8-b817d790&RESPONSE-DATA-FORMAT=XML&REST-PAYLOAD&paginationInput.entriesPerPage=1&keywords=';
+  apiRoot = 'http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=AronHubb-gamefax-PRD-8820665a8-b817d790&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&paginationInput.entriesPerPage=1&keywords=';
   apiEnd= '&categoryId=139973&descriptionSearch=true';
 
   constructor(private http: HttpClient) { }
