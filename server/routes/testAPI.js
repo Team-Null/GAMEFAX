82a6a2461937b855f98c9218c908341eb19bef8f
4D <kyle.leehubbard@gmail.com>
2018-10-02 19:13:43 -0700

    Fixed a space omegalaughing

--- server/routes/testAPI.js
+++ server/routes/testAPI.js
@@ -6,6 +6,6 @@ const PostAPI = 'https://jsonplaceholder.typicode.com/posts';
 // Export function that performs the API call
 module.exports.getTestData = function(gameName) {
     //Console Log to make sure backend is receiving the name of the game.
-    console.log("Test API Game Name:" + gameName)
+    console.log("Test API Game Name: " + gameName)
     return axios.get(`${PostAPI}`)
 }
