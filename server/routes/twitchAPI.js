64b63c957bd409342e4ec865d7b6d7373a0f49f6
4D <kyle.leehubbard@gmail.com>
2018-10-09 19:00:23 -0700

    Async Twitch API Handling
    Updated Twitch API Handlign to include asynchronous functions for good coding practice.

--- server/routes/twitchAPI.js
+++ server/routes/twitchAPI.js
@@ -8,35 +8,39 @@ const clientID = 'yxkhooad1bbvkjb13ibi0hcv4hhlcu';
 var twitchPayload;
 
 //Function to handle HTTP Requests to Twitch's APIs
-module.exports.getTwitchData = function(gameName, callback) {
+module.exports.getTwitchData = async function(gameName, callback) {
     //Clear JSON object before each new game search
     twitchPayload = {};
     twitchPayload["status"] = 0;
     twitchPayload["gameInput"] = gameName;
 
-    //HTTP Request to find Game ID from Game Name
-    axios.get(twitchGamesAPI, {
-        params: {
-           'name': gameName 
-        },
-        headers: {
-            'Client-ID': clientID
-        }
-    })
-    .then(response => {
+    try {
+        var gameResponse = await axios.get(twitchGamesAPI, {
+            params: {
+               'name': gameName 
+            },
+            headers: {
+                'Client-ID': clientID
+            }
+        });
+
         //Throws error if game is not found in Twitch Database
-        if(response.data.data.length == 0) {
+        if(gameResponse.data.data.length == 0) {
             throw "Game not found.";
         }
 
         //Storing Game Name and Game ID in final JSON
         twitchPayload.status = 1;
-        twitchPayload["game_name"] = response.data.data[0].name;
-        twitchPayload["game_id"] = response.data.data[0].id;
-    })
-    .then(() => {
-        //HTTP Request to find up to 5 Top Streams from Game ID
-        axios.get(twitchStreamsAPI, {
+        twitchPayload["game_name"] = gameResponse.data.data[0].name;
+        twitchPayload["game_id"] = gameResponse.data.data[0].id;
+    } catch(error) {
+        //No Games Found Error
+        console.log(error);
+        callback(twitchPayload);
+    }
+
+    try {
+        var streamResponse = await axios.get(twitchStreamsAPI, {
             params: {
                 'game_id': twitchPayload.game_id,
                 'first': 5
@@ -44,50 +48,40 @@ module.exports.getTwitchData = function(gameName, callback) {
              headers: {
                  'Client-ID': clientID
              }     
-        })
-        .then(response2 => {
-            //Throws error if no streams found for requested game
-            if(response2.data.data.length == 0) {
-                throw "No streams found.";
-            }
-
-            twitchPayload.status = 2;
-            twitchPayload["streams"] = response2.data.data;
-        })
-        .then(() => {
-            //Create variable amount of GET requests for each stream in the array and store in promises[]
-            promises = [];
-
-            for(i = 0; i < twitchPayload.streams.length; i++) {
-                promises.push(axios.get(twitchUserAPI, {
-                    params: {
-                        'id': twitchPayload.streams[i].user_id
-                    },
-                    headers: {
-                        'Client-ID': clientID
-                    }
-                }));
-            }
-
-            //Iterate over each promise and store respective data in final JSON
-            axios.all(promises).then(axios.spread((...responses) => {
-                for(i = 0; i < twitchPayload.streams.length; i++) {
-                    twitchPayload.streams[i]["display_name"] = responses[i].data.data[0].display_name;
-                    twitchPayload.streams[i]["profile_image_url"] = responses[i].data.data[0].profile_image_url;
-                }
-
-                callback(twitchPayload);
-            }))
-        })
-        .catch(error => {
-            //No Streams Found Error
-            console.log(error);
-            callback(twitchPayload);
         });
-    })
-    .catch(error => {
-        //No Games Found Error
+
+        //Throws error if no streams found for requested game
+        if(streamResponse.data.data.length == 0) {
+            throw "No streams found.";
+        }
+
+        twitchPayload.status = 2;
+        twitchPayload["streams"] = streamResponse.data.data;
+    } catch(error) {
+        //No Streams Found Error
         console.log(error);
         callback(twitchPayload);
-    });
+    }
+
+    //Create variable amount of GET requests for each stream in the array and store in promises[]
+    promises = [];
+    for(i = 0; i < twitchPayload.streams.length; i++) {
+        promises.push(axios.get(twitchUserAPI, {
+            params: {
+                'id': twitchPayload.streams[i].user_id
+            },
+            headers: {
+                'Client-ID': clientID
+            }
+        }));
+    }
+
+    //Iterate over each promise and store respective data in final JSON
+    axios.all(promises).then(axios.spread((...userResponses) => {
+        for(i = 0; i < twitchPayload.streams.length; i++) {
+            twitchPayload.streams[i]["display_name"] = userResponses[i].data.data[0].display_name;
+            twitchPayload.streams[i]["profile_image_url"] = userResponses[i].data.data[0].profile_image_url;
+        }
+        callback(twitchPayload);
+    }));
 }
