b746e1cf4b15f2e7adf3f97c7da61f4e00bc1c6c
4D <kyle.leehubbard@gmail.com>
2018-11-03 23:25:14 -0700

    Added HTML Collector page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,7 @@ import { WebdataComponent } from './components/webdata/webdata.component';
     YoutubeComponent,
     WalmartComponent,
     TwitterComponent,
-    WebdataComponent,
+    WebdataComponent
   ],
   imports: [
     BrowserModule,
