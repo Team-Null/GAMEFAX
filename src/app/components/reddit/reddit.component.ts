e1d091d6d74e2a3e51159f13e14fa540092ae133
firely111 <firely111@hotmail.com>
2018-11-15 14:29:15 -0800

    Added Subreddit image

--- src/app/components/reddit/reddit.component.ts
+++ src/app/components/reddit/reddit.component.ts
@@ -11,6 +11,7 @@ export class RedditComponent implements OnInit {
   redditData: Object;
   redditDataSearch: Object;
   redditDataSub: Object;
+  iconImage: string;
 
   constructor(private apiController: APIControllerService) { }
 
@@ -19,7 +20,20 @@ export class RedditComponent implements OnInit {
       this.redditData= data;
       this.redditDataSearch= data["data"];
       this.redditDataSub= data["sub"];
+      this.assignImage();
     })
+  }
+
+  assignImage(){
+    if(this.redditDataSub[0].data.icon_img!= ""){
+      this.iconImage= this.redditDataSub[0].data.icon_img;
+    }
+    else if(this.redditDataSub[0].data.community_icon!= ""){
+      this.iconImage= this.redditDataSub[0].data.community_icon;
+    }
+    else{
+      this.iconImage= "https://www.redditstatic.com/new-icon.png";
+    }
   }
 
 }
